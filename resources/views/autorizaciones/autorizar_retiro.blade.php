ce3fe81debae12f90c96add15435abd6f3aab765
magiio <fmaggioni0@gmail.com>
2016-05-15 18:23:43 -0300

    15-05

--- resources/views/autorizaciones/autorizar_retiro.blade.php
+++ resources/views/autorizaciones/autorizar_retiro.blade.php
@@ -6,12 +6,13 @@
 
 
 @section('main-content')
+    <form class="form-inline" role="form">
         <div class="box">
             <div class="box-header">
                 <h3 class="box-title">Despachar autorización</h3>
                  <div class="box-tools">
                     <div class="input-group">
-                        <input type="button" name="table_search" class="form-control input-sm pull-right" value='Descartar autorizacion' />
+                        <input type="button" name="table_search" class="btn btn-danger" value='Descartar autorizacion' />
                     </div>
                 </div>
             </div><!-- /.box-header -->
@@ -21,6 +22,7 @@
                     <tr>
                         <th>Derivado por</th>
                         <th>Area</th>
+                        <th>Responsable</th>
                         <th>Fecha</th>
                         <th>Autoriza a</th>
                         <th>Legajo</th>
@@ -28,8 +30,9 @@
                     <tr>
                         <td>ndalmas0</td>
                         <td>Espacios Verdes</td>
+                        <td>Zolezzi, jose</td>
                         <td>11-7-2016 12:45</td>
-                        <td>Franco Maggioni</td>
+                        <td>Maggioni, Franco</td>
                         <td>47004</td>
                     </tr>
                     </table>
@@ -64,9 +67,11 @@
                         <td><input type='checkbox' /></td>
                     </tr>
                 </table>
-                <input type='button' class="btn btn-default" value='Despachar'/><input type='button' class="btn btn-default" value='Imprimir'/>
-                
+                <div style="padding: 8px;" class="form-group">
+                    <input type='button' class="btn btn-primary" value='Despachar'/>
+                    <input type='submit' class="btn btn-default" value='Imprimir'/>
+                </div>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
-
+    </form>
 @endsection
--- resources/views/salidas/salidas.blade.php
+++ resources/views/salidas/salidas.blade.php
@@ -95,7 +95,61 @@
                     </fieldset>
 
                     </div>
+
+
                 <form>
+            </div><!-- /.box-body -->
+        </div><!-- /.box -->
+
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">Despachar autorización</h3>
+                <div class="form-group">
+                    <label class="col-sm-6">Retira:</label>
+                    <input type="text" value="Maggioni, Franco">
+                </div>
+                 <div class="form-group col-sm-6">
+                    <label>Articulo:</label>
+                    <input type="text" value="Maggioni, Franco">
+                    <label >Stock actual:</label>
+                    <input type="text" value="56" disabled>
+                </div>
+                 <div class="form-group">
+                    <label class="col-sm-6">Cantidad:</label>
+                    <input type="text" value="2">
+                </div>
+            </div><!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+                <table class="table table-hover">
+                    <tr>
+                        <th>Nro Item</th>
+                        <th>Articulo</th>
+                        <th>Cantidad</th>
+                        <th>Autorizar</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>BOTINES DE CUERO PARA OPERARIOS 1 x U.</td>
+                        <td>1</td>
+                        <td><input type='checkbox' /></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>REMERA</td>
+                        <td>1</td>
+                        <td><input type='checkbox' /></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>GUANTE PUÑO LARGO x 1 U</td>
+                        <td>1</td>
+                        <td><input type='checkbox' /></td>
+                    </tr>
+                </table>
+                <div style="padding: 8px;" class="form-group">
+                    <input type='button' class="btn btn-primary" value='Despachar'/>
+                    <input type='submit' class="btn btn-default" value='Imprimir'/>
+                </div>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
 @endsection
